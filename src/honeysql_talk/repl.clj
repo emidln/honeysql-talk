a08e8890eb5fcdaf8eecf9086f0865c5ef2efae5
Brandon Adams <emidln@gmail.com>
2016-07-20 15:30:26 -0500

    Added some notes to the repl experience

--- src/honeysql_talk/repl.clj
+++ src/honeysql_talk/repl.clj
@@ -4,10 +4,12 @@
             [com.stuartsierra.component :as component]
             [clj-time.core :as t]
             clj-time.jdbc
+            [clojure.java.jdbc :refer [query execute!]]
             [honeysql.core :refer [call format raw]]
             [honeysql.helpers :refer :all]
             [honeysql-talk.extra :refer :all]))
 
+;; this is setup for the docker instructions in this project's README.md
 (def db (-> (new-postgres-database {:classname "org.postgresql.Driver"
                                     :subprotocol "postgresql"
                                     :subname "//127.0.0.1:5432/exercises"
@@ -17,9 +19,13 @@
 
 (comment
 
+  ;; using HoneySQL
   (query-runner db (-> (select :*)
                        (from :cd.members)
                        (limit 1)))
+
+  ;; using SQL
+  (query db ["SELECT * FROM cd.members LIMIT 1"])
 
   )
 
